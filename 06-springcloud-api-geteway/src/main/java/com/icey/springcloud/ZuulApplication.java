fb159cb2acd12b73ab71e6e384db7693330ca937
icey <295821075@qq.com>
2020-04-26 00:18:19 +0800

    API zuul

---
+++ 06-springcloud-api-geteway/src/main/java/com/icey/springcloud/ZuulApplication.java
@@ -0,0 +1,15 @@
+package com.icey.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy//#开启Zuul的API网关服务功能
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+}
